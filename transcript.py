0d169c59665b6022eb443755db2f70f7fefa4133
jeffshee <jeffshee8969@gmail.com>
2020-08-28 18:06:26 +0900

    transcript, optimized audio split

--- transcript.py
+++ transcript.py
@@ -12,6 +12,7 @@ import speech_recognition
 from pydub import AudioSegment
 
 from edit_audio import mix_audio, trim_audio
+from split_audio import optimized_segment_audio
 from main_util import cleanup_directory
 
 """
@@ -29,12 +30,18 @@ from main_util import cleanup_directory
 
 """
 
-option = {
-    'retry_num': 3,  # 音声認識が成功するまで試す回数
-    'use_run_speaker_diarization': False,  # run_speaker_diarizationを実行するか、loudness-basedの手法を使用するか
-    'use_spectral_cluster': False,  # run_speaker_diarizationを実行する際、SpectralClustringを使用するか、UIS-RNNを使用するか、
-    'allow_overlapping': False,  # 話者間の対話で音声のOverlapping（重なり）が存在すると想定するか、しないか
-    'use_mixed_audio_during_s2t': True  # S2Tを実行する際、Mix音声を使うか、Pinmic音声を使うか
+# TODO:
+# (1) UIS-RNN causes CUDA OOM, while it's working fine with the source code I had clone from github manually.
+
+config = {
+    'use_run_speaker_diarization': True,  # run_speaker_diarizationを実行するか、loudness-basedの手法を使用するか (False)
+    'use_spectral_cluster': False,  # run_speaker_diarizationを実行する際、SpectralClustringを使用するか、UIS-RNNを使用するか (False)
+
+    'allow_overlapping': False,  # 話者間の対話で音声のOverlapping（重なり）が存在すると想定するか、しないか (False)
+
+    'max_split_duration_sec': 60,  # 音声を分割する際、最大何秒まで許せるか（これより短い音声は統合される） (60)
+    's2t_retry_num': 3,  # 音声認識が成功するまで試す回数 (3)
+    's2t_use_mixed_audio': True,  # 音声認識する際、Mix音声を使うか、Pinmic音声を使うか (True)
 }
 
 
@@ -47,6 +54,9 @@ def transcript(output_dir, audio_path_list, people_num=None):
     :param people_num: people_num for Diarization.
     :return:
     """
+
+    with open(join(output_dir, 'config.txt'), 'w') as f:
+        print(config, file=f)
 
     if people_num is None:
         assert len(audio_path_list) > 1
@@ -68,19 +78,20 @@ def transcript(output_dir, audio_path_list, people_num=None):
     mix_audio(audio_path_list, mixed_audio_path)
 
     """ 1. Diarization """
-    if option['use_run_speaker_diarization']:
+    if config['use_run_speaker_diarization']:
         from run_speaker_diarization_v2 import run_speaker_diarization
         run_speaker_diarization(output_dir + os.sep, mixed_audio_name, diarization_dir + os.sep, people_num=people_num,
-                                overlap_rate=0.5, use_spectral_cluster=option['use_spectral_cluster'])
+                                overlap_rate=0.5, use_spectral_cluster=config['use_spectral_cluster'])
         df_diarization_compact = diarization_compact(join(diarization_dir, 'result.csv'), diarization_dir)
     else:
-        if option['allow_overlapping']:
+        if config['allow_overlapping']:
+            # 現在の実験データでは、まだノイズが酷かったため、この方法はうまくいきません。
             df_diarization_compact = speech_segmenter_only_v2(audio_path_list, diarization_dir)
         else:
             df_diarization_compact = loudness_after_speech_segmenter(audio_path_list, diarization_dir)
 
     """ 2. Split audio after Diarization """
-    if option['use_mixed_audio_during_s2t']:
+    if config['s2t_use_mixed_audio']:
         split_path_list, start_time_list, end_time_list = split_audio_after_diarization(df_diarization_compact,
                                                                                         [mixed_audio_path],
                                                                                         split_audio_dir)
@@ -91,7 +102,7 @@ def transcript(output_dir, audio_path_list, people_num=None):
     """ 3. Segmentation -> Speech2Text """
     output_csv = []
     for i, split_path in enumerate(sorted(split_path_list)):
-        segment_path_list = segment_audio(split_path, segment_audio_dir)
+        segment_path_list = optimized_segment_audio(split_path, segment_audio_dir)
         if len(segment_path_list) == 0:
             continue
         segment_transcript_list = []
@@ -103,32 +114,24 @@ def transcript(output_dir, audio_path_list, people_num=None):
             r = speech_recognition.Recognizer()
             segment_progress = '[{}/{}]'.format(j, len(segment_path_list))
 
-            if AudioSegment.from_file(segment_path).duration_seconds > 60:
-                # Segmentの長さが1分以上の場合、最終手段としてsplit_on_silenceを使って音声を分割する
-                print('=== Duration>60 sec! Use transcript_split_on_silence as last resort ===')
-                split_silence_dir = join(join(output_dir, 'split_silence'), basename(segment_path)[:-4])
-                cleanup_directory(split_silence_dir)
-                segment_transcript_list.append(transcript_split_on_silence(segment_path, split_silence_dir))
-                print(split_progress, segment_progress, segment_path, segment_transcript_list[-1])
-            else:
-                with speech_recognition.AudioFile(segment_path) as src:
-                    audio = r.record(src)
-                    while (not is_success) and (attempt < option['retry_num']):
-                        if attempt > 0:
-                            print('=== Attempt #{} ==='.format(attempt + 1))
-                        try:
-                            segment_transcript_list.append(r.recognize_google(audio, language='ja-JP'))
-                            is_success = True
-                            print(split_progress, segment_progress, segment_path, segment_transcript_list[-1])
-                        except speech_recognition.UnknownValueError:
-                            if attempt == option['retry_num'] - 1:
-                                segment_transcript_list.append('')
-                            print(split_progress, segment_progress, segment_path, 'Could not understand audio')
-                        except speech_recognition.RequestError as e:
-                            if attempt == option['retry_num'] - 1:
-                                segment_transcript_list.append('')
-                            print(split_progress, segment_progress, segment_path, 'RequestError: {}'.format(e))
-                        attempt += 1
+            with speech_recognition.AudioFile(segment_path) as src:
+                audio = r.record(src)
+                while (not is_success) and (attempt < config['s2t_retry_num']):
+                    if attempt > 0:
+                        print('=== Attempt #{} ==='.format(attempt + 1))
+                    try:
+                        segment_transcript_list.append(r.recognize_google(audio, language='ja-JP'))
+                        is_success = True
+                        print(split_progress, segment_progress, segment_path, segment_transcript_list[-1])
+                    except speech_recognition.UnknownValueError:
+                        if attempt == config['s2t_retry_num'] - 1:
+                            segment_transcript_list.append('')
+                        print(split_progress, segment_progress, segment_path, 'Could not understand audio')
+                    except speech_recognition.RequestError as e:
+                        if attempt == config['s2t_retry_num'] - 1:
+                            segment_transcript_list.append('')
+                        print(split_progress, segment_progress, segment_path, 'RequestError: {}'.format(e))
+                    attempt += 1
 
         origin_filename = basename(split_path)
         order, speaker_class = int(origin_filename.split('_')[0]), int(origin_filename.split('_')[2][:-4])
@@ -143,41 +146,6 @@ def transcript(output_dir, audio_path_list, people_num=None):
 
     # Remove `split` directory, we don't need them anymore
     # shutil.rmtree(split_audio_dir)
-
-
-def transcript_split_on_silence(input_path, output_dir):
-    """
-    This is used as the last resort, if the audio is too long (>1 min) even after diarization and segmentation
-    The case might be the speaker is giving a long presentation, etc.
-    """
-    from pydub.silence import split_on_silence
-    full_audio = AudioSegment.from_wav(input_path)
-    dbfs = full_audio.dBFS
-    split_list = split_on_silence(full_audio, min_silence_len=1000, silence_thresh=dbfs - 10, keep_silence=100)
-    origin_filename = basename(input_path)[:-4]  # remove extension
-
-    transcript_list = []
-    for i, split in enumerate(split_list):
-        # Export as file
-        output_path = join(output_dir, '{}_{:03d}.wav'.format(origin_filename, i))
-        split.export(output_path, format='wav')
-
-        attempt = 0
-        is_success = False
-        r = speech_recognition.Recognizer()
-        with speech_recognition.AudioFile(output_path) as source:
-            while (not is_success) and (attempt < option['retry_num']):
-                audio = r.record(source)
-                try:
-                    transcript_list.append(r.recognize_google(audio, language='ja-JP'))
-                    is_success = True
-                except speech_recognition.UnknownValueError:
-                    pass
-                except speech_recognition.RequestError:
-                    pass
-                attempt += 1
-
-    return ' '.join(transcript_list)
 
 
 def diarization_compact(csv_path, output_dir=None):
@@ -216,64 +184,75 @@ def split_audio_after_diarization(df_diarization_compact, audio_path_list, outpu
     return output_path_list, output_start_time_list, output_end_time_list
 
 
-def segment_audio(input_path, output_dir):
-    from inaSpeechSegmenter import Segmenter
-    seg = Segmenter(vad_engine='smn', detect_gender=False)
-    segmentation = seg(input_path)
-    output_path_list = []
-    i = 0
-    for segment in segmentation:
-        if segment[0] == 'speech':  # 音声区間
-            trim_ms_range = (segment[1] * 1000, segment[2] * 1000)
-            origin_filename = basename(input_path)[:-4]  # remove extension
-            output_path = join(output_dir, '{}_seg_{:03d}.wav'.format(origin_filename, i))
-            output_path_list.append(output_path)
-            trim_audio(input_path, output_path, trim_ms_range)
-            i += 1
-    return output_path_list
+# def segment_audio(input_path, output_dir):
+#     from inaSpeechSegmenter import Segmenter
+#     seg = Segmenter(vad_engine='smn', detect_gender=False)
+#     segmentation = seg(input_path)
+#     output_path_list = []
+#     i = 0
+#     for segment in segmentation:
+#         if segment[0] == 'speech':  # 音声区間
+#             trim_ms_range = (segment[1] * 1000, segment[2] * 1000)
+#             origin_filename = basename(input_path)[:-4]  # remove extension
+#             output_path = join(output_dir, '{}_seg_{:03d}.wav'.format(origin_filename, i))
+#             output_path_list.append(output_path)
+#             trim_audio(input_path, output_path, trim_ms_range)
+#             i += 1
+#     return output_path_list
 
 
-# def segment_audio_v2(input_path, output_dir):
-#     # TODO:
-#     max_duration = 60
-#     if AudioSegment.from_file(input_path, format="wav").duration_seconds <= max_duration:
-#         # The audio is short enough, not need to perform segment audio
-#         return [input_path]
-#     else:
-#         from inaSpeechSegmenter import Segmenter
-#         seg = Segmenter(vad_engine='smn', detect_gender=False)
-#         segmentation = seg(input_path)
-#         output_path_list = []
-#         origin_filename = basename(input_path)[:-4]  # remove extension
+# def optimized_segment_audio(input_path, output_dir):
+#     def check_overflow(split_list):
+#         count = 0
+#         total_overflow = 0
+#         max_duration = 60
+#         for start, end in split_list:
+#             if end - start > max_duration:
+#                 count += 1
+#                 total_overflow += (end - start) - max_duration
+#         return count, total_overflow
+#
+#     def get_init_split(segmentation):
+#         split_list = []
+#         prev_point = 0
+#         for result, start, end in segmentation:
+#             if result != 'speech':
+#                 mid_point = start + (end - start) / 2
+#                 split_list += [(prev_point, mid_point)]
+#                 prev_point = mid_point
+#         split_list += [(prev_point, segmentation[-1][2])]
+#         return split_list
+#
+#     def remove_split_at(split_list, index):
+#         front = split_list[:index] if index > 0 else []
+#         back = split_list[index + 2:] if index + 2 < len(split_list) else []
+#         new_split = front + [(split_list[index][0], split_list[index + 1][1])] + back
+#         return new_split
+#
+#     def optimized_split(segmentation):
+#         split = get_init_split(segmentation)
+#         init_count, init_total_overflow = check_overflow(split)
 #         i = 0
-#         cur_start = 0
-#         for segment in segmentation:
-#             if segment[2] > cur_start + max_duration:
-#                 if (segment[2] - segment[1]) <= max_duration:
-#                     output_path = join(output_dir, '{}_seg_{:03d}.wav'.format(origin_filename, i))
-#                     output_path_list.append(output_path)
-#                     trim_audio(input_path, output_path, (cur_start * 1000, segment[1] * 1000))
-#                     cur_start = segment[1]
-#                     i += 1
-#                 else:
-#                     if segment[0] == 'speech':
-#                         output_path = join(output_dir, '{}_seg_{:03d}.wav'.format(origin_filename, i))
-#                         output_path_list.append(output_path)
-#                         trim_audio(input_path, output_path, (cur_start * 1000, segment[1] * 1000))
+#         while i < len(split) - 1:
+#             new_split = remove_split_at(split, i)
+#             new_count, new_total_overflow = check_overflow(new_split)
+#             if new_count == init_count and not new_total_overflow > init_total_overflow:
+#                 split = new_split
+#                 i = 0
+#             i += 1
+#         return split
 #
-#                         output_path = join(output_dir, '{}_seg_{:03d}.wav'.format(origin_filename, i + 1))
-#                         output_path_list.append(output_path)
-#                         trim_audio(input_path, output_path, (segment[1] * 1000, segment[2] * 1000))
-#                         cur_start = segment[2]
-#                         i += 2
-#
-#                     else:
-#                         output_path = join(output_dir, '{}_seg_{:03d}.wav'.format(origin_filename, i))
-#                         output_path_list.append(output_path)
-#                         trim_audio(input_path, output_path, (cur_start * 1000, segment[1] * 1000))
-#                         cur_start = segment[2]
-#                         i += 1
-#
+#     from inaSpeechSegmenter import Segmenter
+#     seg = Segmenter(vad_engine='smn', detect_gender=False)
+#     segmentation = seg(input_path)
+#     output_path_list = []
+#     origin_filename = basename(input_path)[:-4]  # remove extension
+#     for i, (start, end) in enumerate(optimized_split(segmentation)):
+#         trim_ms_range = (start * 1000, end * 1000)
+#         output_path = join(output_dir, '{}_seg_{:03d}.wav'.format(origin_filename, i))
+#         output_path_list.append(output_path)
+#         trim_audio(input_path, output_path, trim_ms_range)
+#         i += 1
 #     return output_path_list
 
 
@@ -490,8 +469,9 @@ if __name__ == "__main__":
     # Prepare short ver. for testing
     # for path in ["test/wave/200225_芳賀先生_実験23/200225_芳賀先生_実験23voice{}.wav".format(i) for i in range(1, 7)]:
     #     trim_audio(path, path[:-4] + '_short.wav', [0, 300000])
-    variant = '_{}'.format(str(option))
+    # variant = '_split_optimized'
+    variant = '_uis_rnn'
     transcript('output_exp22' + variant,
                ["test/wave/200225_芳賀先生_実験22/200225_芳賀先生_実験22voice{}.wav".format(i) for i in range(1, 6)])
-    transcript('output_exp23' + variant,
-               ["test/wave/200225_芳賀先生_実験23/200225_芳賀先生_実験23voice{}.wav".format(i) for i in range(1, 7)])
+    # transcript('output_exp23' + variant,
+    #            ["test/wave/200225_芳賀先生_実験23/200225_芳賀先生_実験23voice{}.wav".format(i) for i in range(1, 7)])
