59bb796f344c7ec5c6b5cd39bf0ca156d095d99a
jeffshee <2Bb&1fvzoPQU>
2020-11-24 16:03:00 +0900

    recognize emotion

--- utils/capture_face_ng.py
+++ utils/capture_face_ng.py
@@ -1,6 +1,9 @@
 import cv2
 from multiprocessing import Process, Manager
 import pandas as pd
+import numpy as np
+from os.path import join
+from ast import literal_eval as make_tuple
 
 
 def get_frame_position(video_capture):
@@ -19,7 +22,7 @@ def get_video_length(video_capture):
     return int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
 
-# TODO: frame_skip, Calculate the bounding box on original resolution
+# TODO: frame_skip, Calculate the bounding box on original resolution, last batch is ditched (bug)
 def detect_face(video_path, gpu_index=0, parallel_num=1, k_resolution=3, batch_size=8, return_dict=None):
     """
     Detect all the faces in video, using CNN and CUDA for high accuracy and performance
@@ -88,7 +91,7 @@ def detect_face(video_path, gpu_index=0, parallel_num=1, k_resolution=3, batch_s
             for frame_number_in_batch, face_locations in enumerate(batch_of_face_locations):
                 face_locations = sorted(face_locations, key=lambda x: x[1])  # Sort the face with left value
                 frame_number = frame_count - batch_size + frame_number_in_batch
-                # Note: frame_number, (top, left, bottom, right), ...
+                # Note: frame_number, (top, right, bottom, left), ...
                 result.append([frame_number, *face_locations])
 
             bar.update(len(frames))
@@ -135,11 +138,71 @@ def detect_face_multiprocess(video_path, parallel_num=3, k_resolution=3, batch_s
     df.to_csv("detect_face.csv")
 
 
-def recognize_emotion():
-    pass
+def get_face_location_from_csv(df, frame_num):
+    face_location = df.loc[df["frame"] == frame_num].values.tolist()
+    if len(face_location) == 0:
+        # frame_num not found in csv
+        return None
+    return [make_tuple(s) for s in face_location[0][2:]]
+
+
+# TODO
+def recognize_emotion(video_path, k_resolution=3, batch_size=128):
+    df = pd.read_csv("detect_face.csv")
+    # Note convert string to tuple
+    # t = df["face1"][0]
+    # print(make_tuple(t))
+    # print(get_face_location_from_csv("detect_face.csv", 110000))
+    from keras.models import model_from_json
+    model_dir = '../model'
+    model_name = 'mini_XCEPTION'
+    model = model_from_json(open(join(model_dir, 'model_{}.json'.format(model_name)), 'r').read())
+    model.load_weights(join(model_dir, 'model_{}.h5'.format(model_name)))
+
+    # Open video file
+    video_capture = cv2.VideoCapture(video_path)
+    original_w, original_h = get_video_dimension(video_capture)
+    resize_rate = (1080 * k_resolution) / original_w  # Resize
+    w = int(original_w * resize_rate)
+    h = int(original_h * resize_rate)
+
+    frames = []
+    faces = []
+
+    frame_count = 0
+    while video_capture.isOpened():
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
+
+        # Bail out when the video file ends
+        if not ret:
+            break
+
+        # Resize
+        frame = cv2.resize(frame, (w, h))
+        face_location = get_face_location_from_csv(df, frame_count)
+        for top, right, bottom, left in face_location:
+            face = frame[top:bottom, left:right]
+            face = cv2.resize(cv2.cvtColor(face, cv2.COLOR_BGR2GRAY), (64, 64)) / 255.0
+            faces.append(face)
+
+        frame_count += 1
+        # faces.append()
+
+        # Save each frame of the video to a list
+        frames.append(frame)
+
+        # Every 128 frames (the default batch size), batch process the list of frames to find faces
+        if len(faces) > batch_size:
+            val = np.stack(faces)
+            predictions = model.predict(val)
+            print(predictions)
+            # Clear the frames array to start the next batch
+            faces = []
 
 
 # # Prepare short ver. for testing
 # from edit_video import trim_video
 # trim_video("../datasets/Videos_new_200929/200221_expt12_video.mp4", ['00:00:00', '00:00:10'], "test.mp4")
-detect_face_multiprocess("test.mp4")
+# detect_face_multiprocess("test.mp4")
+recognize_emotion("test.mp4")
