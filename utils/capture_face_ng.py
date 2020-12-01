4d7e634a186607b0dc76c197c096dadd27a79f59
jeffshee <2Bb&1fvzoPQU>
2020-12-01 16:07:24 +0900

    [detect_face] bug fixes, frame skipping

--- utils/capture_face_ng.py
+++ utils/capture_face_ng.py
@@ -22,8 +22,38 @@ def get_video_length(video_capture):
     return int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
 
 
-# TODO: frame_skip, Calculate the bounding box on original resolution, last batch is ditched (bug)
-def detect_face(video_path, gpu_index=0, parallel_num=1, k_resolution=3, batch_size=8, return_dict=None):
+def calculate_original_box(x1, y1, x2, y2, resized_w, resized_h, original_w, original_h):
+    alpha_x1 = x1 / (resized_w - x1)
+    alpha_x2 = x2 / (resized_w - x2)
+
+    alpha_y1 = y1 / (resized_h - y1)
+    alpha_y2 = y2 / (resized_h - y2)
+
+    original_x1 = alpha_x1 * original_w / (1 + alpha_x1)
+    original_x2 = alpha_x2 * original_w / (1 + alpha_x2)
+
+    original_y1 = alpha_y1 * original_h / (1 + alpha_y1)
+    original_y2 = alpha_y2 * original_h / (1 + alpha_y2)
+
+    return round(original_x1), round(original_y1), round(original_x2), round(original_y2)
+
+
+# TODO
+def interpolate_detect_face_result(detect_face_result):
+    interpolated = []
+    for i in range(len(detect_face_result) - 1):
+        pass
+
+
+# TODO
+# use face_distance, choose closest. If collide, check nearest mid point.
+def match_detect_face_result(detect_face_result):
+    pass
+
+
+# TODO: Initial analysis to detect a box where participants' faces will be in there mostly, cut the computational cost
+def detect_face(video_path, gpu_index=0, parallel_num=1, k_resolution=3, frame_skip=0, batch_size=8,
+                return_dict=None):
     """
     Detect all the faces in video, using CNN and CUDA for high accuracy and performance
     Refer to the example of face_recognition below:
@@ -33,6 +63,7 @@ def detect_face(video_path, gpu_index=0, parallel_num=1, k_resolution=3, batch_s
     :param gpu_index: Which GPU to use
     :param batch_size: Batch size
     :param k_resolution:
+    :param frame_skip:
     :param return_dict:
 
     :return:
@@ -52,13 +83,15 @@ def detect_face(video_path, gpu_index=0, parallel_num=1, k_resolution=3, batch_s
     video_length = get_video_length(video_capture)
     divided = video_length // parallel_num  # Frames per process
     frame_range = [[i * divided, (i + 1) * divided] for i in range(parallel_num)]
-    frame_range[-1][1] = video_length  # Set the rest of the frames to the last process
+    # Set the rest of the frames to the last process
+    # Note: This is actually a workaround, for some reason OpenCV will fail to read the last frame
+    frame_range[-1][1] = video_length - 1
 
     # The frame range of this process
     start, end = frame_range[gpu_index]
 
-    frames = []
-    frame_count = start
+    frame_list = []
+    frame_numbers = []
 
     result = []
 
@@ -66,12 +99,18 @@ def detect_face(video_path, gpu_index=0, parallel_num=1, k_resolution=3, batch_s
 
     set_frame_position(video_capture, start)  # Move position
     while video_capture.isOpened() and get_frame_position(video_capture) in range(start, end):
+        current_pos = get_frame_position(video_capture)
+        next_pos = current_pos + frame_skip + 1
+
         # Grab a single frame of video
         ret, frame = video_capture.read()
+        if frame_skip != 0 and current_pos % frame_skip != 0:
+            bar.update(1)
+            continue
 
-        # Bail out when the video file ends
-        if not ret:
-            break
+        # # Bail out when the video file ends
+        # if not ret:
+        #     break
 
         # Resize
         frame = cv2.resize(frame, (w, h))
@@ -80,29 +119,35 @@ def detect_face(video_path, gpu_index=0, parallel_num=1, k_resolution=3, batch_s
         frame = frame[:, :, ::-1]
 
         # Save each frame of the video to a list
-        frame_count += 1
-        frames.append(frame)
+        frame_list.append(frame)
+        frame_numbers.append(current_pos)
 
         # Every 128 frames (the default batch size), batch process the list of frames to find faces
-        if len(frames) == batch_size:
-            batch_of_face_locations = face_recognition.batch_face_locations(frames, number_of_times_to_upsample=0)
+        if len(frame_list) == batch_size or next_pos >= end:
+            batch_of_face_locations = face_recognition.batch_face_locations(frame_list,
+                                                                            number_of_times_to_upsample=0,
+                                                                            batch_size=len(frame_list))
 
             # Now let's list all the faces we found in all 128 frames
             for frame_number_in_batch, face_locations in enumerate(batch_of_face_locations):
                 face_locations = sorted(face_locations, key=lambda x: x[1])  # Sort the face with left value
-                frame_number = frame_count - batch_size + frame_number_in_batch
+                frame_number = frame_numbers[frame_number_in_batch]
                 # Note: frame_number, (top, right, bottom, left), ...
+                # Scale the box to original resolution
+                face_locations = [calculate_original_box(top, right, bottom, left, w, h, original_w, original_h) for
+                                  top, right, bottom, left in face_locations]
                 result.append([frame_number, *face_locations])
 
-            bar.update(len(frames))
+            bar.update(len(frame_list))
             # Clear the frames array to start the next batch
-            frames = []
+            frame_list = []
+            frame_numbers = []
 
     if return_dict is not None:
         return_dict[gpu_index] = result
 
 
-def detect_face_multiprocess(video_path, parallel_num=3, k_resolution=3, batch_size=8):
+def detect_face_multiprocess(video_path, parallel_num=3, k_resolution=3, frame_skip=3, batch_size=8):
     # Note: Batch size = 8 is about the limitation of current machine
     print("Using", parallel_num, "GPU(s)")
     process_list = []
@@ -114,6 +159,7 @@ def detect_face_multiprocess(video_path, parallel_num=3, k_resolution=3, batch_s
                   "gpu_index": i,
                   "parallel_num": parallel_num,
                   "k_resolution": k_resolution,
+                  "frame_skip": frame_skip,
                   "batch_size": batch_size,
                   "return_dict": return_dict}
 
@@ -179,10 +225,13 @@ def recognize_emotion(video_path, k_resolution=3, batch_size=128):
             break
 
         # Resize
-        frame = cv2.resize(frame, (w, h))
+        # frame = cv2.resize(frame, (w, h))
         face_location = get_face_location_from_csv(df, frame_count)
         for top, right, bottom, left in face_location:
             face = frame[top:bottom, left:right]
+            import matplotlib.pyplot as plt
+            plt.imshow(face)
+            plt.show()
             face = cv2.resize(cv2.cvtColor(face, cv2.COLOR_BGR2GRAY), (64, 64)) / 255.0
             faces.append(face)
 
@@ -204,5 +253,5 @@ def recognize_emotion(video_path, k_resolution=3, batch_size=128):
 # # Prepare short ver. for testing
 # from edit_video import trim_video
 # trim_video("../datasets/Videos_new_200929/200221_expt12_video.mp4", ['00:00:00', '00:00:10'], "test.mp4")
-# detect_face_multiprocess("test.mp4")
+detect_face_multiprocess("test.mp4")
 recognize_emotion("test.mp4")
