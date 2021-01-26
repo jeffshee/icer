47da74eb7d5c240def17518d02ee7987109badab
Touyuki <xiaoshaoly@gmail.com>
2021-01-26 14:34:10 +0900

    new emotion recognition module(make use of the result from capture_face_ng.py and delete the face matching in this part)

--- emotion_recognition_new.py
+++ emotion_recognition_new.py
@@ -19,6 +19,13 @@ def calculate_MAR(landmarks):
     mar = L / D
     return mar
 
+# int型の座標を取得
+def get_coords(p1):
+    try:
+        return int(p1[0][0][0]), int(p1[0][0][1])
+    except IndexError:
+        return int(p1[0][0]), int(p1[0][1])
+
 def get_parts_coordinates(a, b):
     l = list()
     if b == True:
@@ -51,90 +58,226 @@ def get_eye_location(face_landmarks):
     eye_center=  (righteye + lefteye) // 2, (topeye + bottomeye) // 2
     return eye_center
 
-def emotion_recognition(video_path,batch_size,k_resolution,file_path):
-    df = pd.read_csv(file_path) ##读取csv文件
-    # print(df["face1"][3])
-    k=3 ##每3帧检测一次
+def emotion_recognition(target_video_path,path_result,k_resolution,emotions,split_video_index,split_video_num,file_path):
+    with open(file_path+"detect_face.pt", "rb") as f:
+        rst = pickle.load(f)  ##rst[0][3]["face_location"]为第0个人第3次识别时人脸部的坐标
+    person_number = len(rst)  ## 参加的人数
+
+    # k=3 ##每3帧检测一次  ##根据文件中的帧数进行判断
     model_dir = '../model'
     model_name = 'mini_XCEPTION'
     model = model_from_json(open(join(model_dir, 'model_{}.json'.format(model_name)), 'r').read())
     model.load_weights(join(model_dir, 'model_{}.h5'.format(model_name)))##载入模型
 
-    # Open video file
-    video_capture = cv2.VideoCapture(video_path) ##读取视频
-    original_w, original_h = get_video_dimension(video_capture) ##获取视频宽度和长度
-    resize_rate = (1080 * k_resolution) / original_w  # Resize
+    input_movie = cv2.VideoCapture(target_video_path)  # 動画を読み込む
+    video_frame_rate = input_movie.get(cv2.CAP_PROP_FPS)
+    length = int(input_movie.get(cv2.CAP_PROP_FRAME_COUNT))  # 動画の長さを測る
+
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')  # 動画コーデック指定
+    original_w = int(input_movie.get(cv2.CAP_PROP_FRAME_WIDTH))  # 動画の幅を測る
+    original_h = int(input_movie.get(cv2.CAP_PROP_FRAME_HEIGHT))  # 動画の高さを測る
+    resize_rate = (1080 * k_resolution) / original_w  # 動画の横幅を3Kに固定
     w = int(original_w * resize_rate)
     h = int(original_h * resize_rate)
+    output_movie = cv2.VideoWriter(join(path_result, 'output.avi'), fourcc, video_frame_rate, (w, h))  # 出力する動画の詳細を設定する
 
-    frames = []
-    faces = []
-    time_frame=[]
-    count=0
-    last_count=0
-    frame_count = 0
-    start_frame=0
-    emotion = {}
-    eye_center={}
-    mouse_opening_rate_list={}
-    person_number=3 ## 参加的人数
-    for i in range(0,person_number):
-        emotion[i]={}
-        eye_center[i]={}
-        mouse_opening_rate_list[i]={}
-    while video_capture.isOpened():
-        # Grab a single frame of video
-        ret, frame = video_capture.read() ##从视频流中读取一帧
+    # CSVファイルの最初の列
+    csv_saving_list = [[["frame_number", "time(ms)", "prediction", "x", "y", "absolute_x", "relative_x", "x_movement",
+                         "absolute_y", "relative_y", "y_movement", "mouse opening", "gesture", "top", "bottom", "left",
+                         "right"]] for _ in range(person_number)]
+    # 各変数を初期化
+    # 前フレームの顔の座標を記録する変数
+    rec_face = [[0, 0, 0, 0]] * person_number
+    # 顔の中心を示す変数
+    # 顔の中央のオプティカルフロー計算に用いる変数
+    p0 = [np.array([0, 0])] * person_number
+    p1 = [np.array([0, 0])] * person_number
+    # 首を動かしたかどうかを判断する変数
+    gesture = [0] * person_number
+    # オプティカルフローの移動量
+    x_movement = [0] * person_number
+    y_movement = [0] * person_number
+    # 座標
+    x = [0] * person_number
+    y = [0] * person_number
+    # 動作判定の表示時間
+    gesture_show_frame = 30
+    gesture_show = [gesture_show_frame] * person_number
+    # 顔が認識できたかどうか
+    face_recog_flag = [False] * person_number
+    # 初めて顔を認識したかどうか
+    first_face_recog_flag = [False] * person_number
+    # オプティカルフローの前フレームからの移動量の絶対値
+    absolute_x = [0] * person_number
+    absolute_y = [0] * person_number
+    # オプティカルフローの前フレームからの移動量の相対値
+    relative_x = [0] * person_number
+    relative_y = [0] * person_number
+    # 目の中心位置座標
+    eye_center = [[0, 0]] * person_number
+    # 感情
+    emotion = ["Unknown"] * person_number
+    mouse_opening_rate_list = [0] * person_number
 
-        # Bail out when the video file ends
+    frame_duration = length // split_video_num
+    start_frame = split_video_index * frame_duration
+    stop_frame = length if split_video_index == split_video_num - 1 else start_frame + frame_duration
+    input_movie.set(cv2.CAP_PROP_POS_FRAMES, start_frame)  # 動画の開始フレームを設定
+    first_flag = True
+    file_strat_frame=0 ##初始化文件中最开始的一帧
+    file_frame_count=0
+    for i in range (0,len(rst[0])):
+        if rst[0][i]["frame_number"]>=start_frame:
+            file_strat_frame=rst[0][i]["frame_number"]  ##记录从哪帧开始  第一个0位人数的索引 表示第0个人
+            file_frame_count=i  ##记录索引
+
+    for frame_number in range(start_frame, stop_frame): ##从开始帧数到结束的帧数
+        # 動画を読み込む
+        ret, frame = input_movie.read()
+        print("\nWriting frame {} / {}".format(frame_number, length - 1))
+        # 動画が読み取れない場合は終了
         if not ret:
             break
 
-        face_location = get_face_location_from_csv(df, frame_count)  ##获取某一帧的所有信息 形成一个list
-        # print(face_location)
+        # フレームのサイズを調整する
+        frame = cv2.resize(frame, (w, h))
 
-        for top, right, bottom, left in face_location:
-            face = frame[top:bottom, left:right]
-            import matplotlib.pyplot as plt
-            plt.imshow(face)
-            plt.show()
-            face = cv2.resize(cv2.cvtColor(face, cv2.COLOR_BGR2GRAY), (64, 64)) / 255.0
-            faces.append(face)
-        frame_count += 3 ##3帧进行一次表情识别
-        count +=1 ##进行一次计数
-        # Save each frame of the video to a list
-        frames.append(frame)
+        if frame_number!= file_strat_frame:
+            continue  ##先调到与文件时间同步
+        # 表情识别  头部判断
+        else:
+            for person_index in range(person_number):
+                top, right, bottom, left=rst[person_index][file_frame_count]["face_location"]
+                rec_face[face_index] = [top, bottom, left, right]
+                # 顔領域を切り取る
+                dst = frame[top:bottom, left:right]
+                # 表情認識
+                if model_name == "mini_XCEPTION":
+                    val = cv2.resize(cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY), (64, 64)) / 255.0
+                    val = val.reshape(1, 64, 64, 1)
+                else:
+                    exit("Invalid model_name")
 
-        # #save the time
-        # time_frame.append(frame_count) ##计算时间
+                # Solved https://stackoverflow.com/questions/60905801/parallelizing-model-predictions-in-keras-using-multiprocessing-for-python
+                if (rst[person_index][file_frame_count]["detective"]):
+                    predictions = model.predict(val, batch_size=1)  # 学習モデルから表情を特定
+                    emotion[face_index] = emotions[int(np.argmax(predictions[0]))]
+                    box_label(frame, left, top, right, bottom, "{}:{}".format(face_index, emotion[face_index]),
+                              color=(0, 255, 0))
+                    face_recog_flag[face_index] = True
 
-        # Every 128 frames (the default batch size), batch process the list of frames to find faces
-        if count%128==0:
-            val = np.stack(faces)
-            predictions = model.predict(val)
-            # print(predictions)
-            # Clear the frames array to start the next batch
-            emotion_count=0##从0开始计数
-            for i in range(last_count,count): ##帧数
-                time_frame.append(start_frame + i * k)##计算当前帧的时间
-                for j in range(0,person_number): ##人数
-                    j_location=get_face_location_from_csv(df,start_frame+i*k)[j]
-                    if j_location!="Unknow":
-                        emotion[j][start_frame + i * k]=predictions[emotion_count]
+                # 顔の部位の座標を検出する
+                face_landmarks_list = face_recognition.face_landmarks(dst, face_locations=[
+                    (0, dst.shape[1], dst.shape[0], 0)])  # 顔の部位の座標が取れないときは return []
+                # 顔の部位を切り取る．画像中に複数顔が存在する場合を想定して返り値がlistになっている．
+                for face_landmarks in face_landmarks_list:
+                    # 独自の計算式から口の空き具合を計算
+                    mouse_opening_rate_list[face_index] = calculate_MAR(face_landmarks)
 
-                        ##判断眼睛坐标
-                        face_landmarks = face_recognition.face_landmarks(face[emotion_count], face_locations=[
-                            (0, face[emotion_count].shape[1], face[emotion_count].shape[0], 0)])
-                        tmp_eye_center=get_eye_location(face_landmarks)
-                        eye_center[j][start_frame + i * k]=j_location[3]+tmp_eye_center[0],j_location[0]+tmp_eye_center[1]
-                        mouse_opening_rate_list[j][start_frame + i * k] =calculate_MAR(face_landmarks)
-                        emotion_count=emotion_count+1
-                    else:
-                        emotion[j][start_frame + i * k]="Unknow"
-                        eye_center[j][start_frame + i * k]=0,0  ##无法识别表情的时候不写眼的坐标
-                        mouse_opening_rate_list[j][start_frame + i * k]=0
-            last_count=count
-            start_frame=start_frame+(count-last_count)*k
-            faces = []
+                    # 目の位置
+                    leye_x = get_parts_coordinates(face_landmarks["left_eye"], True)
+                    reye_x = get_parts_coordinates(face_landmarks["right_eye"], True)
+                    eye_y = get_parts_coordinates(face_landmarks["left_eye"] + face_landmarks["right_eye"], False)
+                    topeye = min(eye_y)
+                    bottomeye = max(eye_y)
+                    lefteye = min(leye_x)
+                    righteye = max(reye_x)
+                    eye_center[face_index] = left + (righteye + lefteye) // 2, top + (topeye + bottomeye) // 2
+
+                if not first_face_recog_flag[face_index]:
+                    # face_center[face_index] = (left + right) / 2, (top + bottom) / 2
+                    p0[face_index] = np.array([[eye_center[face_index]]], np.float32)
+                    first_face_recog_flag[face_index] = True
+            # 顔が読み取れなかった場合 感情＝不明とする
+            for face_index, flag in enumerate(face_recog_flag):
+                if not flag:
+                    emotion[face_index] = "Unknown"
+
+            #头部的判断
+            frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            for face_index in range(person_number):
+                if first_flag:  # 前フレームがない場合は終了
+                    break
+                elif first_face_recog_flag[face_index]:
+                    p1[face_index] = np.array([[eye_center[face_index]]], np.float32)
+                    if face_recog_flag[face_index]: ##
+                        p1[face_index] = np.array([[eye_center[face_index]]], np.float32)
+                    else: ##此部分的代碼需要討論
+                        # オプティカルフローのパラメータ設定
+                        lk_params = dict(winSize=(int(rec_face[face_index][1] - rec_face[face_index][0]),
+                                                  int(rec_face[face_index][1] - rec_face[face_index][0])), maxLevel=2,
+                                         criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+                        p1[face_index], st, err = cv2.calcOpticalFlowPyrLK(frame_gray_old, frame_gray, p0[face_index],
+                                                                           None, **lk_params)
+                    x[face_index] = get_coords(p0[face_index])[0]
+                    y[face_index] = get_coords(p0[face_index])[1]
+
+                    a, b = get_coords(p0[face_index]), get_coords(p1[face_index])
+                    absolute_x[face_index] = abs(a[0] - b[0])
+                    absolute_y[face_index] = abs(a[1] - b[1])
+                    relative_x[face_index] = a[0] - b[0]
+                    relative_y[face_index] = a[1] - b[1]
+                    decay_number = int((rec_face[face_index][1] - rec_face[face_index][0]) * 0.05)
+                    x_movement[face_index] = max(0, int((x_movement[face_index] + abs(a[0] - b[0])) - decay_number))
+                    y_movement[face_index] = max(0, int((y_movement[face_index] + abs(a[1] - b[1])) - decay_number))
+
+                    gesture_threshold = int((rec_face[face_index][1] - rec_face[face_index][0]) * 0.2)
+
+                    if y_movement[face_index] > gesture_threshold:
+                        gesture[face_index] = 1
+                        y_movement[face_index] = 0
+                        gesture_show[face_index] = gesture_show_frame  # number of frames a gesture is shown
+
+                    if gesture[face_index] and gesture_show[face_index] > 0:
+                        cv2.circle(frame, get_coords(p1[face_index]), 100, (0, 0, 255), 5)  # now nodding
+                        gesture_show[face_index] -= 1
+
+                    if gesture_show[face_index] <= 0:
+                        gesture[face_index] = 0
+
+                    # 計算した座標に点を表示
+                    cv2.circle(frame, get_coords(p1[face_index]), 3, (255, 0, 255), -1)
+                    cv2.putText(frame, "{}".format(gesture_threshold), (face_index * 100, 30), cv2.FONT_HERSHEY_COMPLEX,
+                                1.0, (0, 0, 0), 2)
+                    cv2.putText(frame, "{}".format(y_movement[face_index]), (face_index * 100, 60),
+                                cv2.FONT_HERSHEY_COMPLEX, 1.0, (0, 0, 0), 2)
+
+                    p0[face_index] = p1[face_index]
+                else:
+                    pass
+            frame_gray_old = frame_gray
+
+            # データをリストに追加
+            for face_index in range(len(known_faces)):
+                csv_saving_list[face_index].append([
+                    frame_number, int(1000 * frame_number / video_frame_rate),
+                    emotion[face_index],
+                    x[face_index], y[face_index],
+                    absolute_x[face_index], relative_x[face_index], x_movement[face_index],
+                    absolute_y[face_index], relative_y[face_index], y_movement[face_index],
+                    mouse_opening_rate_list[face_index],
+                    gesture[face_index],
+                    rec_face[face_index][0], rec_face[face_index][1], rec_face[face_index][2], rec_face[face_index][3]
+                ])
+
+            face_recog_flag = [False] * len(target_image_path_list)
+            output_movie.write(frame)
+            first_flag = False
+
+        file_strat_frame=rst[0][file_frame_count+1]["frame_number"]
+        file_frame_count=file_frame_count+1
+    # 動画保存
+    input_movie.release()
+    cv2.destroyAllWindows()
+
+    # 結果をCSVファイルに保存
+    for i in range(len(target_image_path_list)):
+        with open(join(path_result, 'result{}.csv'.format(i)), "w", encoding="Shift_jis") as f:
+            writer = csv.writer(f, lineterminator="\n")  # writerオブジェクトの作成 改行記号で行を区切る
+            writer.writerows(csv_saving_list[i])  # csvファイルに書き込み
 
 # a=emotion_recognition("file/out1.mp4",3,128,"file/detect_face.csv")
+if __name__ == '__main__':
+    with open("detect_face.pt", "rb") as f:
+        rst = pickle.load(f)
+        print(rst[0]["face_locations"])## 脸部坐标
