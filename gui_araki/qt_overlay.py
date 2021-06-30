79ec4f1d6acb2393ac46e89828f655f2d31e0e26
xiaoshaoly@gamil.com <xiaoshaoly@gmail.com>
2021-06-30 09:58:02 +0900

    debug0630

--- gui_araki/qt_overlay.py
+++ gui_araki/qt_overlay.py
@@ -517,8 +517,9 @@ class EmotionStatisticsWidget(QWidget):
             face_and_index_img = np.array(
                 fig.canvas.renderer.buffer_rgba())
             face_and_index_img = cv2.cvtColor(face_and_index_img, cv2.COLOR_RGBA2BGR)
+            cv2.imwrite('test0623.png', face_and_index_img)
             self.image_label.setPixmap(QPixmap(face_and_index_img))
-            cv2.imwrite('test0623.png',face_and_index_img)
+            self.image_label.setScaledContents(True)
             self.mpl_widget.draw()
 
 
