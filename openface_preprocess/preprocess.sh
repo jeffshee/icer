9e6c9a4afd181fc49b4f40614b0f6d56c3f91809
Suke-H <hondii1997@gmail.com>
2021-10-27 09:49:02 +0900

    openfaceサービス化修正

--- openface_preprocess/preprocess.sh
+++ openface_preprocess/preprocess.sh
@@ -1,17 +1,25 @@
 #!/bin/sh -x
+# 実行時に"sh -x ./preprocess.sh"とすることで実行状況がわかりやすくなる
 
 export DATA_MOUNT=$(pwd)/data
 mkdir -p $DATA_MOUNT/processed # this is just to ensure this is writable by user
 docker-compose up -d openface && sync # sync is to wait till service starts
 
-# # For many faces
-# build/bin/FaceLandmarkVidMulti -f "/docker_video_dir/video.avi"
-# # For single face
-# build/bin/FeatureExtraction -f "/docker_video_dir/video.avi"
+for file in `\find $DATA_MOUNT -name '*.avi'`; do
+    mkdir -p $DATA_MOUNT/processed/$(basename $file .avi)
+    # For single face
+    # docker exec -it openface FeatureExtraction -f $file -out_dir $DATA_MOUNT/processed/$(basename $file .avi)
+    # For many faces
+    docker exec -it openface FaceLandmarkVidMulti -f $file -out_dir $DATA_MOUNT/processed/$(basename $file .avi)
+done
 
-# For many faces
-docker exec -it openface FaceLandmarkVidMulti -f $DATA_MOUNT/test.mp4 -out_dir $DATA_MOUNT/processed
-# # For single face
-# build/bin/FeatureExtraction -f $DATA_MOUNT/test.mp4 -out_dir $DATA_MOUNT/processed
+for file in `\find $DATA_MOUNT -name '*.mp4'`; do
+    mkdir -p $DATA_MOUNT/processed/$(basename $file .mp4)
+    # For single face
+    # docker exec -it openface FeatureExtraction -f $file -out_dir $DATA_MOUNT/processed/$(basename $file .mp4)
+    # For many faces
+    docker exec -it openface FaceLandmarkVidMulti -f $file -out_dir $DATA_MOUNT/processed/$(basename $file .mp4)
+done
+
 docker exec -it openface chown -R $(id -u):$(id -u) $DATA_MOUNT # chown to current user
 docker-compose down # stop service if you wish
